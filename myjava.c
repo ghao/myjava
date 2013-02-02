a8aceda24057738f55e11ee3c18f5fe9f6cab156
colorful <332234055@qq.com>
2013-02-02 15:56:53 +0800

    ghao

---
+++ myjava.c
@@ -0,0 +1,4 @@
+# this a personal progress record during the period of me leraning Java@2013,for a new deepth.
+# thougt it has so much errors in this file,it means what im thingking,come on!
+# to be NO.1
+# 2013.02.02
